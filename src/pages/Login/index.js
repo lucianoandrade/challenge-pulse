433af37f1f1944b9b1eb6aa35742b02729670f90
Luciano Andrade <landrade@gruporcp.local>
2021-02-01 20:04:33 -0300

    realizacao de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, {useState} from "react";
+import { Link, useHistory } from "react-router-dom";
 import PageContainer from "../../components/features/PageContainer/PageContainer";
 import Input from "../../components/elements/Input";
 import Button from "../../components/elements/Button";
@@ -8,11 +7,41 @@ import Button from "../../components/elements/Button";
 import { Container, Title, Form, Links } from "./styles";
 
 function Login() {
+
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const userActive = JSON.parse(localStorage.getItem('userActive')) || [];
+  const allUsers = JSON.parse(localStorage.getItem('users')) || [];
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(senha.length < 6) {
+      setError(true);
+      setErrorMessage("Mínimo 6 caracteres");
+      setSenha("");
+    } else {
+      // eslint-disable-next-line
+      allUsers.map(e => {
+        if(e.email === email && e.senha === senha) {
+          userActive.push({name: e.name, email: e.email})
+          localStorage.setItem('userActive', JSON.stringify(userActive));
+          history.push("/");
+        } else {
+          setError(true);
+          setErrorMessage("E-mail ou senha incorreto");
+        }
+      });
+    }
+  }
+
   return (
     <PageContainer>
       <Container>
         <Title>Acesse sua conta</Title>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Input 
             id="email"
             type="email"
@@ -20,14 +49,20 @@ function Login() {
             placeholder="Ex: seunome@email.com"
             label="E-mail"
             required={true}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <Input 
-              id="senha"
-              type="password"
-              name="senha"
-              placeholder="Sua Senha"
-              label="Senha"
-              required={true}
+            id="senha"
+            type="password"
+            name="senha"
+            placeholder="Sua Senha"
+            label="Senha"
+            required={true}
+            error={error}
+            errorMessage={errorMessage}
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
           />
           <Button>Entrar</Button>
         </Form>
